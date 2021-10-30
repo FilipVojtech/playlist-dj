fc9d2b44763e28081443aa6fdfe38734ad1b6a9b
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2021-10-30 22:11:05 +0200

    Add database to health check

--- PlaylistDJ.API/src/controllers/ApiController.ts
+++ PlaylistDJ.API/src/controllers/ApiController.ts
@@ -5,6 +5,11 @@ const router = require('express').Router()
 /**
  * Return API status
  */
-router.get('/', async (req: Request, res: Response) => res.status(200).json({ status: 'running' }))
+router.get('/', async (req: Request, res: Response) => {
+    res.status(200).json({
+        api: { status: 'running' },
+        database: { status: 'running' },
+    })
+})
 
 export const apiController = router
