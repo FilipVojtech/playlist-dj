b06b82d72aa0be609526fbdb142ddd9bb694052c
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2021-12-28 22:59:38 +0100

    Hide navigation
    Hide navigation in Playlist and in Settings

--- PlaylistDJ.Frontend/src/pages/Playlist.svelte
+++ PlaylistDJ.Frontend/src/pages/Playlist.svelte
@@ -1,10 +1,15 @@
 <script lang='ts'>
     import Title from '../components/Title.svelte'
+    import { onDestroy, onMount } from 'svelte'
+    import { showNav } from '../Utility/stores'
 
     export let params = {
         id: '',
     }
     let id = params.id
+
+    onMount(() => $showNav = false)
+    onDestroy(() => $showNav = true)
 </script>
 
 <Title />
--- PlaylistDJ.Frontend/src/pages/Settings.svelte
+++ PlaylistDJ.Frontend/src/pages/Settings.svelte
@@ -3,6 +3,11 @@
     import Footer from '../components/Footer.svelte'
     import { ChevronLeftIcon } from 'svelte-feather-icons'
     import { pop } from 'svelte-spa-router'
+    import { onDestroy, onMount } from 'svelte'
+    import { showNav } from '../Utility/stores'
+
+    onMount(() => $showNav = false)
+    onDestroy(() => $showNav = true)
 </script>
 
 <Title iconBefore={ChevronLeftIcon} onClickBefore={pop} text='Settings' />
