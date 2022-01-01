c5ffe001742375129ea3b880e28329990c7fde35
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-01-01 21:37:07 +0100

    Create language picker
    Took 1 hour 43 minutes

---
+++ PlaylistDJ.Frontend/src/components/LanguagePicker.svelte
@@ -0,0 +1,43 @@
+<script lang='ts'>
+    import { _, locale, locales } from 'svelte-i18n'
+    import { GlobeIcon } from 'svelte-feather-icons'
+
+    // Format the locale value before binding
+    $locale = $locale?.indexOf('-') === -1 ? $locale : $locale?.substring(0, $locale?.indexOf('-'))
+</script>
+
+<main class='option'>
+    <span class='option__icon'>
+        <GlobeIcon size='30' />
+    </span>
+    <select bind:value={$locale} class='select' id='language-select' name='language'>
+        {#each $locales as loc}
+            <option value={loc}>{$_(`locales.${loc}`)}</option>
+        {/each}
+    </select>
+</main>
+
+<style>
+    .option {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 15px 10px;
+
+        background-color: var(--lighter-bg);
+        border-radius: 20px;
+    }
+
+    .option__icon {
+        margin-right: 10px;
+    }
+
+    .select {
+        background-color: var(--darker-bg);
+        border: 1px solid var(--darker-bg);
+        border-radius: 15px;
+        color: white;
+        padding: 5px;
+        width: 100%;
+    }
+</style>
--- PlaylistDJ.Frontend/src/pages/Settings.svelte
+++ PlaylistDJ.Frontend/src/pages/Settings.svelte
@@ -1,6 +1,5 @@
 <script lang='ts'>
-    import Title from '../components/Title.svelte'
-    import Footer from '../components/Footer.svelte'
+    import { Footer, LanguagePicker, Title } from '../components'
     import { ChevronLeftIcon } from 'svelte-feather-icons'
     import { pop } from 'svelte-spa-router'
     import { onDestroy, onMount } from 'svelte'
@@ -14,5 +13,8 @@
 <svelte:head>
     <title>{$_('page.settings.title')}</title>
 </svelte:head>
-<Title iconBefore={ChevronLeftIcon} onClickBefore={pop} text={$_('page.settings.title')} />
-<Footer />
+<main>
+    <Title iconBefore={ChevronLeftIcon} onClickBefore={pop} text={$_('page.settings.title')} />
+    <LanguagePicker />
+    <Footer />
+</main>
