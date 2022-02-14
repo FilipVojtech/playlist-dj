e91a3b7624e4f741f28da9e233542c752bb00439
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-02-14 14:15:16 +0100

    Add mail update frequency enum

--- PlaylistDJ.API/src/entities/index.ts
+++ PlaylistDJ.API/src/entities/index.ts
@@ -2,3 +2,13 @@ export * from './BaseEntity'
 export * from './Filter'
 export * from './Playlist'
 export * from './User'
+
+// export enum UpdateFrequency {
+//     Disabled = 0,
+//     Daily = 1,
+//     Weekly = 7,
+//     BiWeekly = 14,
+//     Monthly = 30,
+//     // Custom
+// }
+// Custom amount disabled because I don't want to mess with it
