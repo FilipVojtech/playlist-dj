c17fc8a0d681f82532d1fe602df495721c692b20
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-03-14 18:03:06 +0100

    Add DB connection options to MikroORM config
    Took 2 minutes

--- PlaylistDJ.API/src/mikro-orm.config.ts
+++ PlaylistDJ.API/src/mikro-orm.config.ts
@@ -3,7 +3,7 @@ import { Options } from '@mikro-orm/core'
 export default {
     entities: ['./dist/entities'],
     entitiesTs: ['./src/entities'],
-    dbName: 'playlist-dj',
     type: 'mongo',
-    clientUrl: 'mongodb://127.0.0.1:27017/',
+    dbName: '',
+    clientUrl: '',
 } as Options
