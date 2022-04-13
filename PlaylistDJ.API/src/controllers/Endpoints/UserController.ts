3240030fd54b64ef287798b35f33723b06be1532
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-04-13 05:06:33 +0200

    Make delete account endpoint work

--- PlaylistDJ.API/src/controllers/Endpoints/UserController.ts
+++ PlaylistDJ.API/src/controllers/Endpoints/UserController.ts
@@ -14,9 +14,14 @@ router.route('/account')
         //todo: Post account
         res.sendStatus(501)
     })
-    .delete((req: Request, res: Response) => {
-        // todo: Delete account
-        res.sendStatus(501)
+    .delete(async (req: Request, res: Response) => {
+        res.sendStatus(200)
+        // Remove user content
+        for (let playlist of await DI.playlistRepository.find({ owner: req.session.user!._id.toString() }))
+            await DI.playlistRepository.removeAndFlush(playlist)
+        //Lastly, remove user
+        await DI.userRepository.nativeDelete({ _id: req.session.user!._id })
+        await DI.userRepository.flush()
     })
 
 router.route('/profile')
