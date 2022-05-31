7f218fd009a247451e06ad09f506cd49b457c501
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-05-31 14:31:45 +0200

    Add properties to filter entity

--- PlaylistDJ.API/src/entities/Filter.ts
+++ PlaylistDJ.API/src/entities/Filter.ts
@@ -1,5 +1,6 @@
 import { Entity, Property } from '@mikro-orm/core'
 import { BaseEntity } from './'
+import { FilterType } from '@playlist-dj/types'
 
 @Entity()
 export class Filter extends BaseEntity {
@@ -9,8 +10,21 @@ export class Filter extends BaseEntity {
     @Property()
     name: string
 
-    constructor(name: string) {
+    @Property()
+    type: FilterType
+
+    @Property()
+    spotifyId: string
+
+    /**
+     * @param name
+     * @param type
+     * @param id - Spotify ID of the item
+     */
+    constructor(name: string, type: FilterType, id: string) {
         super()
         this.name = name
+        this.type = type
+        this.spotifyId = id
     }
 }
