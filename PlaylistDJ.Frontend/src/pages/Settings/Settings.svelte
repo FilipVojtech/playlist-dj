dfc1a190336932272fe13b83693ada2611cd9e9e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-06-17 15:10:21 +0200

    Reformatting

--- PlaylistDJ.Frontend/src/pages/Settings/Settings.svelte
+++ PlaylistDJ.Frontend/src/pages/Settings/Settings.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
     import { Header, LanguagePicker, ListItem } from '../../components'
     import { BellIcon, LogOutIcon, UserIcon } from 'svelte-feather-icons'
     import { _ } from 'svelte-i18n'
@@ -7,20 +7,21 @@
 <svelte:head>
     <title>{$_('page.settings.title')}</title>
 </svelte:head>
-    <Header text={$_('page.settings.title')} />
-<div class='options'>
-    <div class='options__list'>
+
+<Header text={$_('page.settings.title')} />
+<div class="options">
+    <div class="options__list">
         <!--App - Start app on screen-->
         <!--Display name, show account info, delete account:-->
-        <ListItem href='/settings/account' icon={UserIcon} text={$_('page.settings.account.title')} />
+        <ListItem href="/settings/account" icon={UserIcon} text={$_('page.settings.account.title')} />
         <!--Legal - Legal documents and other stuff-->
-        <ListItem href='/settings/notifications' icon={BellIcon} text={$_('page.settings.communication.title')} />
+        <ListItem href="/settings/notifications" icon={BellIcon} text={$_('page.settings.communication.title')} />
         <LanguagePicker />
     </div>
     <footer>
         <!--Log out button-->
-        <a class='item item--interactive item--warning' href='/logout' style='padding: 10px 10px'>
-            <span class='item__icon'><svelte:component size='25' this='{LogOutIcon}' /></span>
+        <a class="item item--interactive item--warning" href="/logout" style="padding: 10px 10px">
+            <span class="item__icon"><svelte:component this={LogOutIcon} size="25" /></span>
             {$_('app.logout')}
         </a>
     </footer>
