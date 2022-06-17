f6e75b941b066aab0ce948e1a040b93ced947305
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-06-17 15:00:13 +0200

    Create component for Filter images

---
+++ PlaylistDJ.Frontend/src/components/FilterImg.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import FilterPlaceholder from './FilterPlaceholder.svelte'
+    import { SvelteComponent } from 'svelte'
+
+    export let images: []
+    export let name = ''
+    export let alt = `${name} cover art`
+    export let placeholderIcon: SvelteComponent
+</script>
+
+{#if images.length === 3}
+    <img class="filter__img" src={images[2].url} {alt} />
+{:else if images.length === 1}
+    <img class="filter__img" src={images[0].url} {alt} />
+{:else}
+    <FilterPlaceholder icon={placeholderIcon} />
+{/if}
