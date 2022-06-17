7416115d2192d11b839f39c28c8e620eaf644c0a
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-06-17 15:41:57 +0200

    Add playlist property to Express.Request

--- PlaylistDJ.API/src/global.d.ts
+++ PlaylistDJ.API/src/global.d.ts
@@ -1,9 +1,10 @@
 import * as Express from 'express'
 import * as ExpressSession from 'express-session'
-import { User } from './entities'
+import { Playlist, User } from './entities'
 
 export interface Request extends Express.Request {
     session: Session
+    playlist?: Playlist
 }
 
 interface Session extends ExpressSession.Session {
