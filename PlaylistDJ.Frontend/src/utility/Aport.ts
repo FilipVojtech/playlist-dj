fb2635db42724b1af50e6f9cf05e05b5f99eb762
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-06-17 15:28:10 +0200

    Reformat

--- PlaylistDJ.Frontend/src/utility/Aport.ts
+++ PlaylistDJ.Frontend/src/utility/Aport.ts
@@ -1,12 +1,10 @@
 export default function aport(url: RequestInfo, options?: RequestInit): Promise<Response> {
-    return fetch(url, options)
-        .then(value => {
-            if (value.status === 401) window.location.replace('/logout')
-            else if (value.redirected) {
-                const url = new URL(value.url)
-                if (url.searchParams.has('url'))
-                    window.location.href = url.searchParams.get('url') as string
-            }
-            return value
-        })
+    return fetch(url, options).then(value => {
+        if (value.status === 401) window.location.replace('/logout')
+        else if (value.redirected) {
+            const url = new URL(value.url)
+            if (url.searchParams.has('url')) window.location.href = url.searchParams.get('url') as string
+        }
+        return value
+    })
 }
