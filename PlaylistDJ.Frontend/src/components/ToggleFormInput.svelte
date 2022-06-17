31079a547b571ae87bf312fafddd88d9ef7cc610
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github>
2022-06-17 15:14:59 +0200

    Create toggle form input

---
+++ PlaylistDJ.Frontend/src/components/ToggleFormInput.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    export let checked = false
+    export let name = ''
+    export let id = ''
+</script>
+
+<label class="switch form__input">
+    <input type="checkbox" {name} {id} on:click on:change bind:checked />
+    <span class="slider round" />
+</label>
+
+<style>
+    /* The switch - the box around the slider */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    /* The slider */
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
+
+    .slider:before {
+        position: absolute;
+        content: '';
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
+
+    input:checked + .slider {
+        background-color: var(--main);
+    }
+
+    input:focus + .slider {
+        box-shadow: 0 0 1px var(--main);
+    }
+
+    input:checked + .slider:before {
+        transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+        border-radius: 34px;
+    }
+
+    .slider.round:before {
+        border-radius: 50%;
+    }
+</style>
