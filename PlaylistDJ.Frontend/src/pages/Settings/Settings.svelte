9d4f0493b2f6bcfe109790c8e898c9672cf8147e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-07-01 18:40:06 +0200

    Change link to go to gdpr route

--- PlaylistDJ.Frontend/src/pages/Settings/Settings.svelte
+++ PlaylistDJ.Frontend/src/pages/Settings/Settings.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Header, LanguagePicker, ListItem } from '../../components'
     import { BellIcon, LogOutIcon, PaperclipIcon, UserIcon } from 'svelte-feather-icons'
-    import { _ } from 'svelte-i18n'
+    import { _, locale } from 'svelte-i18n'
 </script>
 
 <svelte:head>
@@ -15,7 +15,11 @@
         <!--Display name, show account info, delete account:-->
         <ListItem href="/settings/account" icon={UserIcon} text={$_('page.settings.account.title')} />
         <ListItem href="/settings/notifications" icon={BellIcon} text={$_('page.settings.communication.title')} />
-        <ListItem href="/settings/legal" icon={PaperclipIcon} text={$_('page.settings.legal.title')} />
+        <!--<ListItem href="/settings/legal" icon={PaperclipIcon} text={$_('page.settings.legal.title')} />-->
+        <a class="item item--interactive" href="/gdpr/{$locale}">
+            <span class="item__icon"><PaperclipIcon size="25" /></span>
+            {$_('page.settings.legal.title')}
+        </a>
         <LanguagePicker />
     </div>
     <footer>
