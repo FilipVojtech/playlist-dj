9779032e1a73801dbd220405b097db890e7f195c
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-07-10 01:59:48 +0200

    Add TOS and Privacy policy to Legal page

--- PlaylistDJ.Frontend/src/pages/Settings/Legal.svelte
+++ PlaylistDJ.Frontend/src/pages/Settings/Legal.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
     import Header from '../../components/Header.svelte'
-    import { ChevronLeftIcon } from 'svelte-feather-icons'
+    import { ChevronLeftIcon, PaperclipIcon } from 'svelte-feather-icons'
     import { push } from 'svelte-spa-router'
-    import { _ } from 'svelte-i18n'
+    import { _, locale } from 'svelte-i18n'
 </script>
 
 <Header iconBefore={ChevronLeftIcon} onClickBefore={() => push('/settings')} text={$_('page.settings.legal.title')} />
+
+<a class="item item--interactive" href="/tos/{$locale}">
+    <span class="item__icon"><PaperclipIcon size="25" /></span>
+    {$_('page.settings.legal.tos')}
+</a>
+<a class="item item--interactive" href="/privacy/{$locale}">
+    <span class="item__icon"><PaperclipIcon size="25" /></span>
+    {$_('page.settings.legal.privacyPolicy')}
+</a>
