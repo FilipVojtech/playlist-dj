feea8d9c3b39cc4e3fdcdd354887c2ecbc16efbc
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-07-17 23:08:54 +0200

    Create modals to edit playlist

---
+++ PlaylistDJ.Frontend/src/components/modals/EditPlaylistDetailsModal.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition'
+    import { _ } from 'svelte-i18n'
+    import { closeModal } from 'svelte-modals'
+    import aport from '../../utility/Aport'
+    import { modalEvent } from '../../utility/stores'
+
+    export let isOpen: boolean
+    export let id: string
+    export let name: string = ''
+    export let description: string = ''
+
+    function handleSubmit() {
+        aport(`/api/playlist/${id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, description }),
+        }).then(() => ($modalEvent = 'detailsChange'))
+        closeModal()
+    }
+</script>
+
+{#if isOpen}
+    <div role="dialog" class="modal" transition:fade>
+        <form class="form modal__contents">
+            <div class="modal__title">{$_('modal.editPlaylistDetails.title')}</div>
+            <form class="form" on:submit|preventDefault={handleSubmit}>
+                <label class="form__label" for="playlistName">{$_('modal.editPlaylistDetails.playlistName')}</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="playlistName"
+                    maxlength="100"
+                    required
+                    placeholder={$_('modal.editPlaylistDetails.playlistName')}
+                    class="form__input"
+                    autocomplete="off"
+                    bind:value={name}
+                />
+                <label for="description">{$_('modal.editPlaylistDetails.playlistDescription')}</label>
+                <!--suppress CheckEmptyScriptTag -->
+                <textarea
+                    name="description"
+                    id="description"
+                    maxlength="300"
+                    rows="6"
+                    placeholder={$_('modal.editPlaylistDetails.playlistDescription')}
+                    class="form__input textarea textarea--no-resize"
+                    bind:value={description}
+                    required
+                    on:keydown={e => {
+                        if (e.key === 'Enter' || e.key === 'Return') handleSubmit()
+                    }}
+                />
+                <div class="modal__actions">
+                    <input type="submit" class="actions__button" value={$_('app.save')} />
+                    <button class="actions__button" on:click|preventDefault={closeModal}>{$_('app.cancel')}</button>
+                </div>
+            </form>
+        </form>
+    </div>
+{/if}
---
+++ PlaylistDJ.Frontend/src/components/modals/EditPlaylistPictureModal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition'
+    import { _ } from 'svelte-i18n'
+
+    export let isOpen: boolean
+</script>
+
+{#if isOpen}
+    <div role="dialog" class="modal" transition:fade>
+        <div class="modal__contents">
+            <div class="modal__title">{$_('modal.editPlaylistPicture.title')}</div>
+            <div class="modal__actions">
+                <input type="submit" class="actions__button" value={$_('app.save')} />
+                <button class="actions__button">{$_('app.cancel')}</button>
+            </div>
+        </div>
+    </div>
+{/if}
