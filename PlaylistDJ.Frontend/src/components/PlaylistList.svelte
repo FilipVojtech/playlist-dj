69791728a77c070b1ee2dfbf261b73f9b1efac68
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2022-08-02 17:43:38 +0200

    Make playlist list comp. emit click events

--- PlaylistDJ.Frontend/src/components/PlaylistList.svelte
+++ PlaylistDJ.Frontend/src/components/PlaylistList.svelte
@@ -4,13 +4,17 @@
     import { ListIcon } from 'svelte-feather-icons'
     import Filter from './Filter.svelte'
     import { push } from 'svelte-spa-router'
+    import { createEventDispatcher } from 'svelte'
 
     export let playlists: PDJ.Playlist[] = []
     export let slim: boolean = false
     export let half: boolean = false
+    export let useCustom: boolean = false
+
+    const dispatch = createEventDispatcher()
 </script>
 
-{#each playlists as { name, images, id }}
+{#each playlists as { name, images, id } (id)}
     <Filter
         {name}
         {images}
@@ -21,6 +25,7 @@
         altSubject="playlist cover art"
         placeholderIcon={ListIcon}
         interactive
-        onClick={() => push(`/playlist/${id}`)}
+        onClick={() => (useCustom ? dispatch('click', { id, type: FilterType.Playlist }) : push(`/playlist/${id}`))}
+        on:click
     />
 {/each}
